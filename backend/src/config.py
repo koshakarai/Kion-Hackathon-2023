e24afe6e0dbd1cc60ca942999192dbe972696e63
koshakarai <akay.webdevelopment@gmail.com>
2023-03-05 01:04:07 +0400

    Base backend architecture (2 of 3). edits manage.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,6 +4,7 @@ import os
 
 from starlette.config import Config
 
+
 SRC_DIR = os.path.dirname(os.path.dirname(__file__))
 ENV_PATH = os.path.join(SRC_DIR, ".env")
 # '/home/<user>/<...>/Kion/backend' | root of backend
@@ -13,13 +14,22 @@ ENV_PATH = os.path.join(SRC_DIR, ".env")
 # Config will be read from environment variables and/or ".env" files.
 config = Config(ENV_PATH)
 
+
+# project info
 PROJECT_NAME = config("PROJECT_NAME")
 VERSION = config("VERSION")
- 
-# Debug | SET FALSE IN PRODUCTION!
+
+
+# Debug | make sure disable in production!
 DEBUG = config("DEBUG", cast=bool, default=False) 
 
+
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=CommaSeparatedStrings)
+# CORS_ORIGIN_...
+
+
+# Uvicorn settings
+RELOAD = config("RELOAD", cast=bool, default=False)
 ...
 
 if __name__ == '__main__':
--- backend/src/manage.py
+++ backend/src/manage.py
@@ -5,8 +5,14 @@
 """
 
 import sys
-import uvicorn
+
+import typing
 import colorama
+
+import uvicorn
+
+import config
+
 
 try:
     from asgi import app
@@ -14,13 +20,20 @@ except ImportError as exc:
     raise ImportError("Couldn't import FastApi Instance.")
 
 
-def execute(options):
-    host = "127.0.0.1"
-    port = 80
-    uvicorn.run("manage:app", host=host, port=port, reload=True)
+def start(options: typing.List[str]) -> None:
+    """ Start fastapi on uvicorn asgi web server
+
+    Args:
+        options (typing.List[str]): a list containing the host and port. Implicit default ["127.0.0.1", "80"]
+    """    
+    
+    host, port = options if options else [ "127.0.0.1", "80" ]
+    port = int(port)
+    
+    uvicorn.run("manage:app", host=host, port=port, reload=config.RELOAD)
 
 action_map = {
-    "start": execute,
+    "start": start,
 }
 
 if __name__ == "__main__":
@@ -29,8 +42,7 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     action = args[0]
     options = args[1:]
-    print(args, action, options) # delete after tests
-    
+
     try:
         action_map[action](options)
     except Exception as exc:
